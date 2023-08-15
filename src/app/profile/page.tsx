df0f280993dff1267cd5cd7903e07985a27407eb
anjana12-09 <anjanadhakal09@gmail.com>
2023-08-15 10:13:31 +0545

    add middleware and helpers

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -15,8 +15,10 @@ export default function ProfilePage() {
       console.log("error.message");
       toast.error("error.message");
     }
-  };
+  }
 
+
+  
   return (
     <div className="flex flex-col items-center justify-center max-h-screen py-2">
       <h1>Profile</h1>
